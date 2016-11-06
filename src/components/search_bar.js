b43d140e6fc98e2d679d261289bb054e1ed0b138
Sam Russell <samrussell63@gmail.com>
2016-11-06 12:31:18 -0800

    adding components

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+// import react libraries
+import React from 'react';
+
+const SearchBar = () => {
+    return <input />;
+};
+
+// export so other files can import
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-// Create new component.  This component should produce some html
+// import js module coming from node_modules
 
-const App = function() {
-    return <div>Hi!</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// importing from our file need to give reference
+import SearchBar from './components/search_bar';
+
+const API_KEY = '';
+
+// Create new component.  This component should produce some html
+// when not using arguments can replace function with =>
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 };
 
 // Take this component's generated HTML and put it on the page (In the DOM)
 
+ReactDOM.render(<App />, document.querySelector('.container'));
