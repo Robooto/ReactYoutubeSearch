d68375573e550f0857781d6744a677c8a9075ae4
Sam Russell <samrussell63@gmail.com>
2016-11-09 22:28:36 -0800

    adding new components

---
+++ src/components/video_list.js
@@ -0,0 +1,20 @@
+// won't use state can be function component
+// props come over as an argument
+import React from 'react';
+
+import VideoListItem from './video_list_item.js';
+
+const VideoList = (props) => {
+    // need a key for this list for preformance
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video} />
+    });
+    
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 // import js module coming from node_modules
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 
 // importing from our file need to give reference
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = '';
 
 // Create new component.  This component should produce some html
 // when not using arguments can replace function with =>
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 };
 
 // Take this component's generated HTML and put it on the page (In the DOM)
